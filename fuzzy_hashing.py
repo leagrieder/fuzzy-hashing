0db287fe561e7f83fcca7d21da9134e694bc7812
leagrieder <lea@grieder.org>
2024-02-18 16:50:41 +0100

    template for code

---
+++ fuzzy_hashing.py
@@ -0,0 +1,11 @@
+def biometric_hashing(data):
+    # Implementation of algorithm of Figure 1
+    pass
+
+def main():
+    data = "biometric data"
+    hash = biometric_hashing(data)
+    print(f"Hashed data: {hash}")
+
+if __name__ == "__main__":
+    main()

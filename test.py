86341dfb0d872c9354ea83bd2c1698367e8f1654
leagrieder <lea@grieder.org>
2024-06-01 16:28:57 +0200

    added new images and observations on experiment results

--- test.py
+++ test.py
@@ -23,3 +23,24 @@ print(p2)
 
 p3 = p - delta_diff/2
 print(p3)
+
+covAC = delta_indep*(1/2 - p)/n
+print(covAC)
+
+variance_p = 0.0000252
+variance_delta_diff = 0.0000458
+variance_delta_same = 0.0001058
+
+component0= (p - delta_same/2)/(p + delta_same/2)
+print(component0)
+
+component1 = ((-4*delta_same)/((2*p + delta_same)**3))*variance_p
+print(component1)
+
+component3 = ((8*p)/((2*p + delta_same)**3))*variance_delta_same
+print(component3)
+
+component4 = 2*(2*(2*p - delta_same)/((2*p + delta_same)**3))*covAC
+print(component4)
+
+print(component0 + 1/2*(2*component1 + component3 + 2*component4))

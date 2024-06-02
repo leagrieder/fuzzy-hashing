3a88b68ca6ac240cdd7b29f495adfe9c127d7a4d
leagrieder <lea@grieder.org>
2024-06-02 10:26:41 +0200

    m

--- test.py
+++ test.py
@@ -2,45 +2,51 @@ import math
 
 p = 0.03514
 formula = 2 * p * (1 - p)
-print(formula)
 
 
 n = 90240
 variance_delta = 4*p*(1-p)*(1-2*p*(1-p))/n 
-print(variance_delta)
 
 stdev_delta = math.sqrt(variance_delta)
-print(stdev_delta)
 
 delta_indep = 0.06781
 delta_same = 0.04549
 delta_diff = 0.05988
 p1 = 1 - p - delta_diff/2
-print(p1)
 
 p2 = delta_diff/2
-print(p2)
 
 p3 = p - delta_diff/2
-print(p3)
 
-covAC = delta_indep*(1/2 - p)/n
-print(covAC)
+covAC_indep = delta_indep*(1/2 - p)/n
 
 variance_p = 0.0000252
 variance_delta_diff = 0.0000458
 variance_delta_same = 0.0001058
 
-component0= (p - delta_same/2)/(p + delta_same/2)
+covAB_same = 0.000022
+covAB_diff = 0.000006
+covAC_same = 0.000036
+covAC_diff = 0.000026
+covBC_same = 0.000036
+covBC_diff = 0.000027
+
+component0= (p - delta_diff/2)/(p + delta_diff/2)
 print(component0)
 
-component1 = ((-4*delta_same)/((2*p + delta_same)**3))*variance_p
+component1 = ((-4*delta_diff)/((2*p + delta_diff)**3))*variance_p
 print(component1)
 
-component3 = ((8*p)/((2*p + delta_same)**3))*variance_delta_same
+component3 = ((8*p)/((2*p + delta_diff)**3))*variance_delta_diff
 print(component3)
 
-component4 = 2*(2*(2*p - delta_same)/((2*p + delta_same)**3))*covAC
+component4 = 2*(((-4)*delta_diff)/((2*p + delta_diff)**3))*covAB_diff
 print(component4)
 
-print(component0 + 1/2*(2*component1 + component3 + 2*component4))
+component5 = 2*(2*(2*p - delta_diff)/((2*p + delta_diff)**3))*covAC_diff
+print(component5)
+
+component6 = 2*(2*(2*p - delta_diff)/((2*p + delta_diff)**3))*covBC_diff
+print(component6)
+
+print(component0 + 1/2*(2*component1 + component3 + component4 + component5 + component6))
